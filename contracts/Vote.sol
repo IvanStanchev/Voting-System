5160bd489f1e398f9dd97e387a6a4bedf39073e4
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-01-07 22:59:47 +0200

    Import Ownable contract

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.17;
 pragma abicoder v2;
 
-contract Vote {    
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Vote is Ownable {    
     uint8 public choices;
     uint public endTimestamp; 
 
