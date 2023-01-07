4cae369c4ead0ed39b95548e0b5cb6068fdae4dc
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-01-07 19:39:56 +0200

    Changes in Vote contract

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,30 +1,38 @@
-//SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 pragma abicoder v2;
 
 contract Vote {    
     uint8 public choices;
+    uint public endTimestamp; 
 
     event Winner(uint8);
 
-    mapping(address => bool) public voters; // voters addresses and voting status 
-    mapping(uint8 => uint24) public votes; // votes for each party/politician
+    // Mapping of voter address to voted choice
+    // Voting choices ids start from 1
+    mapping(address => uint8) public voterChoices;
 
-    constructor(uint8 _choices){
+    //Mapping of choice id to number of votes for it
+    mapping(uint8 => uint24) public votes;
+
+    constructor(uint8 _choices, uint daysAfter){
         choices = _choices;
+        endTimestamp = block.timestamp + daysAfter * 1 days;
     }
 
     function vote(uint8 _choiceId) public {
-        require(voters[msg.sender] == false, "You have already voted");
-        voters[msg.sender] = true;
+        require((_choiceId > 0) && (_choiceId <= choices), "Invalid choice");
+        require(voterChoices[msg.sender] == 0, "You have already voted");
+        require(block.timestamp < endTimestamp, "Voting has closed");
+        voterChoices[msg.sender] = _choiceId;
         votes[_choiceId] += 1;
     }
 
     receive() external payable {
-        require(payable(msg.sender).send(msg.value), "Transaction failed");
-    }    
-
+        revert("Error: receive function cannot be called.");
+    }
+      
     fallback() external payable {
-        require(payable(msg.sender).send(msg.value), "Transaction failed");
+        revert("Error: fallback function cannot be called.");
     }
 }
