f4a1b93bccf2bf129d68ef3dd1aa6b4ceb72ccbe
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-01-22 22:42:07 +0200

    Add more networks and etherscan API key to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,46 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config("./.env");
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const privateKey = process.env.privateKey;
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    goerli: {
+      url: process.env.infuraGoerliUrl,
+      accounts: [privateKey],
+    },
+    sepolia: {
+      url: process.env.infuraGoerliUrl,
+      accounts: [privateKey],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.apiKey,
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
