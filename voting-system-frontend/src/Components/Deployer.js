d9b4e269841e8b8fa168748f11632356b41dfe67
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-03-19 14:27:51 +0200

    Add title above new elections form

--- voting-system-frontend/src/Components/Deployer.js
+++ voting-system-frontend/src/Components/Deployer.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { ethers } from 'ethers';
-import { useState } from 'react';
 import VoteDeployer from "../ABI/VoteDeployer.json";
 import './../css/Deployer.css'
 
 const VOTE_DEPLOYER_ADDRESS = "0xc64Bc354F8450aEC8765f29cE73d1e4790619F07";
 
-
-
 function Deployer() {
-    const [provider] = useState(null);
 
     async function deployVoteContract(choices, daysAfter) {
         let provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -23,13 +19,14 @@ function Deployer() {
     return (
         <div>
             <main className='backapp'>
+                <h4>Add new elections</h4>
                 <form>
                     <input id={"choices-count"} placeholder={"Choices count"} /><br />
                     <input id={"days-after"} placeholder={"To end in ... days"} /><br />
                     <button type="button" onClick={async () => {
                         const choices = document.getElementById("choices-count").value;
                         const daysAfter = document.getElementById("days-after").value;
-                        const voteAddr = await deployVoteContract(choices, daysAfter);
+                        const voteAddr = await deployVoteContract(choices, daysAfter)
                     }}>Add elections</button>
                 </form>
                 <hr />
